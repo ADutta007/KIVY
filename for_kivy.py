f93fabcae68ebfa5a06b3e86b9a489da366459a1
ADutta007 <55627806+ADutta007@users.noreply.github.com>
2020-07-12 00:20:58 +0545

    Add files via upload

---
+++ for_kivy.py
@@ -0,0 +1,100 @@
+MyKivyFile='''
+#: import MDRaisedButton kivymd.uix.button.MDRaisedButton
+#: import MDTextField kivymd.uix.textfield.MDTextField
+#: import GridLayout kivymd.uix.gridlayout.GridLayout
+#: import MapView kivy_garden.mapview.MapView
+#: import MapMarker kivy_garden.mapview.MapMarker
+#: import storagepath plyer.storagepath
+#: import vibrator plyer.vibrator
+#: import gps plyer.gps
+#: import toast kivymd.toast.toast
+
+
+
+WindowManager:
+    MainWindow:
+    SecondWindow:
+    ThirdWindow:
+    forkivymd:
+
+<MainWindow>:
+
+
+    name: "main"
+    GridLayout:
+        orientation:"horizontal"
+        padding:60                       
+        MDTextField:
+            hint_text:"Code"
+            fill_color:(0,4,2,1)
+            pos_hint:{'center_x':0.5,'center_y':0.5}
+            icon_left: 'key-variant'
+            icon_right: 'eye-off'
+            helper_text_mode: "on_focus"
+            id: passw
+            max_text_length: 6
+            multiline: False
+            password: True
+            text_color:(236/255.,98/255.,81/255.,1)  
+            on_text_validate:
+                app.root.current = "second" if passw.text == "ashish" else "main"  
+            on_double_tap:
+                app.root.current = "second" if passw.text == "ashish" else "main"
+            
+       
+            
+
+
+
+<SecondWindow>:
+    name: "second"
+
+    GridLayout:
+        cols:2
+        Button:
+            text:"MAP"
+            on_release:
+                app.root.current = "map"
+
+        Button:
+            text:"Functions"
+            on_release:
+                app.root.current="function"
+
+<ThirdWindow>:
+    name:"map"
+    MapView:       
+        id:ktm
+        lat:27.6922368
+        lon:85.3344256
+        zoom:14
+        double_tap_zoom:True
+        MapMarkerPopup:
+            source:"gps_logo.png"
+            lat:27.6922368
+            lon:85.3344256
+            MDRaisedButton:
+                text:"Go bAck!"                       
+                on_release:    
+                    print(ktm.get_bbox())
+                    gps.configure(27,85)
+                    gps.start()                
+                    
+            
+<forkivymd>:
+    name:"function"
+    BoxLayout:
+        orientation: 'vertical'
+        spacing: dp(20)
+        MDRaisedButton:
+            text:"Vibrate"
+            on_release:
+                vibrator.vibrate(time=4)
+        MDRaisedButton:
+            text:"Go bAck!"
+            on_release:
+                app.root.current="second"
+                root.manager.transition.direction = "down"
+
+'''
+
