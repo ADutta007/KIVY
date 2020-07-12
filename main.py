e68a6e9b7f8309a7070e34910767d9dbf99832e6
ADutta007 <55627806+ADutta007@users.noreply.github.com>
2020-07-12 18:21:07 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+import kivy
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from for_kivy import MyKivyFile
+from kivy_garden.mapview import MapView
+from kivy.logger import Logger
+
+from plyer import gps
+
+
+
+class MainWindow(MDScreen):
+    pass
+
+class SecondWindow(MDScreen):
+    pass
+
+
+class ThirdWindow(MDScreen):
+    pass
+
+class Map(MapView):
+    pass
+class WindowManager(ScreenManager):
+    pass
+
+
+
+class forkivymd(MDScreen):
+
+    pass
+
+
+class MyMainApp(MDApp):
+
+
+    def on_start(self):
+        Logger.info("Called Start!")
+        gps.configure(on_location=self.on_gps_location)
+        gps.start()
+
+    def on_gps_location(self,**kwargs):
+
+       Logger.info("Called on Location")
+       Logger.info(kwargs)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.root_widget = Builder.load_string(MyKivyFile)
+
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+
+        return self.root_widget
+
+
+
+if __name__ == "__main__":
+    MyMainApp().run()
